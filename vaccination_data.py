ca1f425fcfd32eddc233daeb6512617716b52178
parijatprasun <parijatprasun@gmail.com>
2021-06-07 01:12:28 +0530

    Update script to poll data and plot

--- vaccination_data.py
+++ vaccination_data.py
@@ -1,11 +1,12 @@
 # import requests
 # from datetime import timedelta, datetime
 import pandas as pd
-import streamlit as st
+import matplotlib.pyplot as plt
+# import streamlit as st
  
 def main():
     # headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36'}
-        
+    pd.set_option('display.max_columns', None, 'display.max_colwidth', None, 'display.max_rows', None)    
     src_url = 'https://github.com/owid/covid-19-data/blob/master/public/data/vaccinations/vaccinations.csv'
     src_ind_url = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/country_data/India.csv'
 
@@ -13,9 +14,12 @@ def main():
     # print(result.status_code)
     # print(result.content)
     df = pd.read_csv(src_ind_url)
-    df2 = pd.DataFrame(df, columns=['date', 'people_fully_vaccinated'])
-    st.title('Vaccination Worm Plot')
-    st.line_chart(df2)
+    df2 = pd.DataFrame(df, columns=['date', 'people_vaccinated'])
+    print(df2)
+    df2.plot(x='date', y='people_vaccinated')
+    plt.show()
+    # st.title('Vaccination Worm Plot')
+    # st.line_chart(df2)
 
 if __name__ == '__main__':
     main()
