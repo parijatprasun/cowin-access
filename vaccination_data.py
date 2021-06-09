b009fd1e0290a4abd3fe4e0437e4cedb0463e9d1
Parijat Prasun Pal <parijatprasun@gmail.com>
2021-06-10 01:06:41 +0530

    Remove so far from output text

--- vaccination_data.py
+++ vaccination_data.py
@@ -38,9 +38,9 @@ def main():
     new_deadline_obj = last_date_obj + timedelta(days=balance_days)
     new_deadline = datetime.strftime(new_deadline_obj, '%d %B %Y')
     last_date_disp = last_date_obj.strftime('%d %B %Y')
-    print(f'Until {last_date_disp} India vaccinated {single_so_far:,} so far out of {target_population:,} with only single dose.')
+    print(f'Until {last_date_disp} India vaccinated {single_so_far:,} out of {target_population:,} with only single dose.')
     print(f'India need {balance_population:,} in {duration} days @ {rvr:,} per day.')
-    print(f'Current vaccination rate (last {block} days average): {cvr:,} per day.')
+    print(f'Current vaccination rate (last {block} days\' average): {cvr:,} per day.')
     print(f'India may get only {max_possible:,} and will trail by {trail:,}.')
     print(f'India may reach target population by {new_deadline} at this rate.')
 
