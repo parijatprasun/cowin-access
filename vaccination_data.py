cb74219713e919e05500d07bd4589c2fd026838a
parijatprasun <parijatprasun@gmail.com>
2021-06-09 15:52:27 +0530

    Modify vacciantion data script

--- vaccination_data.py
+++ vaccination_data.py
@@ -1,12 +1,12 @@
 # import requests
-# from datetime import timedelta, datetime
+from datetime import timedelta, datetime
 import pandas as pd
 import matplotlib.pyplot as plt
 # import streamlit as st
  
 def main():
     # headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36'}
-    pd.set_option('display.max_columns', None, 'display.max_colwidth', None, 'display.max_rows', None)    
+    # pd.set_option('display.max_columns', None, 'display.max_colwidth', None, 'display.max_rows', None)    
     src_url = 'https://github.com/owid/covid-19-data/blob/master/public/data/vaccinations/vaccinations.csv'
     src_ind_url = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/country_data/India.csv'
 
@@ -14,12 +14,48 @@ def main():
     # print(result.status_code)
     # print(result.content)
     df = pd.read_csv(src_ind_url)
-    df2 = pd.DataFrame(df, columns=['date', 'people_vaccinated'])
-    print(df2)
-    df2.plot(x='date', y='people_vaccinated')
-    plt.show()
-    # st.title('Vaccination Worm Plot')
-    # st.line_chart(df2)
+    df['daily_count_d1'] = df['people_vaccinated'].diff()
+    # for index, row in df.iterrows():
+    #     df['daily_count_d1'][index] = df['people_vaccinated'][index] - df['people_vaccinated'][index-1]
+    print(df.tail(10))
+    # print(df.dtypes)
+    # print(df.describe())
+    last_date = df['date'].iloc[-1]
+    last_date_obj = datetime.strptime(last_date, '%Y-%m-%d')
+    # print(last_date_obj)
+    deadline = datetime.strptime('31-12-2021', '%d-%m-%Y')
+    duration = (deadline - last_date_obj).days
+    # print(duration)
+    single_so_far = df['people_vaccinated'].iloc[-1]
+    target_population = 1000000000
+    balance_population = target_population - single_so_far
+    rvr = round(balance_population / duration)
+    block = 10
+    cvr = round(df['daily_count_d1'].tail(block).mean())
+    max_possible = cvr * duration
+    trail = target_population - max_possible
+    balance_days = round(balance_population / cvr)
+    new_deadline_obj = last_date_obj + timedelta(days=balance_days)
+    new_deadline = datetime.strftime(new_deadline_obj, '%d %B %Y')
+    print(f'India vaccinated {single_so_far:,} so far with only single dose')
+    print(f'India need {balance_population:,} in {duration} days @ {rvr:,} per day')
+    print(f'Current vaccination rate (last {block} days average): {cvr:,}')
+    print(f'At this rate India may get only {max_possible:,} and will trail by {trail:,} and may reach target by {new_deadline}')
 
+    """ # fig = plt.figure(figsize=(16,9), dpi=300, facecolor=None, edgecolor=None, frameon=False)
+    df['daily_count_d1'].plot(x=df['date'], kind='bar', color='orange')
+    df['people_vaccinated'].plot(x=df['date'], secondary_y=True, color='green')
+    plt.title('India Vaccination Trend', loc='left', fontname='Arial', fontsize=24, fontweight='bold')
+    plt.xlabel('Days', fontname='Arial', fontsize=12, fontweight='bold')
+    plt.ylabel('Population', fontname='Arial', fontsize=12, fontweight='bold')
+    plt.xticks(fontname='Arial', fontsize=10, rotation='vertical')
+    plt.yticks(fontname='Arial', fontsize=10)
+    plt.ticklabel_format(axis='y', style='plain', scilimits=None, useOffset=None)
+    # df.plot.bar('date', 'daily_count_d1', color='blue', linestyle='solid', ax=ax)
+    plt.tight_layout()
+    plt.show()
+    # plt.savefig('file.png')
+    plt.close() """
+    
 if __name__ == '__main__':
     main()
